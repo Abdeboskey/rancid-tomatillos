2216af9596c5e506aa34c9ccef756e6bb7ef66ea
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-08-19 17:53:40 -0600

    Create render method and jsx for form #13

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react'
+import '../assets/clapboard.png'
 
 class Login extends Component {
   constructor() {
     super()
     this.state = {
-      
+      username: '',
+      password: ''
     }
   }
 
+  render() {
+    return (
+      <>
+        <img src='../assets/clapboard.png' alt='Classic Film-Production Clapboard' />
+        <form>
+          <input
+            type='text'
+            name='username'
+            placeholder='Username'
+            value={this.state.username}
+            />
+          <input
+            type='text'
+            name='password'
+            placeholder='Password'
+            value={this.state.password}
+          />
+          <button>Sign In!</button>
+        </form>
+      </>
+    )
+  }
 }
 
 export default Login

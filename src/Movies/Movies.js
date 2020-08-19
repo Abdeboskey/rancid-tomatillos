46240f395d8d05d542d68f4f7db9b9293f9c7126
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-08-18 20:59:57 -0600

    Create initial functional component #7

---
+++ src/Movies/Movies.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Movies.css'
+
+const Movies = () => {
+	return (
+		<section className='Movies-container'>
+			Movie Cards
+		</section>
+	)
+}
+
+export default Movies

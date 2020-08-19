b7d546fbcb56b562284e0f1f149c1a8debe1066d
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-08-18 21:24:18 -0600

    Add initial fake cards #8

--- src/Movies/Movies.js
+++ src/Movies/Movies.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Card from '../Card/Card'
 import './Movies.css'
 
 const Movies = () => {
 	return (
 		<section className='Movies-container'>
-			Movie Cards
+			<Card title='Brave Little Toaster' averageRating='8' poster='https://image.tmdb.org/t/p/original//sA154deR0X51EcR2lm2FfDczryg.jpg'/>
+			<Card title='Movie2' averageRating='10' poster='https://image.tmdb.org/t/p/original//eDnHgozW8vfOaLHzfpHluf1GZCW.jpg'/>
+			<Card title='Akira' averageRating='10000' poster='https://image.tmdb.org/t/p/original//5KlRFKKSbyCiyYpZSS3A6G5bW0K.jpg'/>
 		</section>
 	)
 }

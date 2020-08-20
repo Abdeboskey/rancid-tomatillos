d6564c890b233b10ffbeb58ad245d19ae48057d9
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-08-20 15:44:27 -0600

    Clean up white space and add regex to execution #21

--- src/Card/Card.test.js
+++ src/Card/Card.test.js
@@ -6,8 +6,7 @@ import '@testing-library/jest-dom'
 describe('Card Component', () => {
 
 	it('Should show the correct content when rendered', () => {
-		// make sure there is a title, poster and rating
-		// setup
+		
 		render(
 			<Card
 				poster='https://images.unsplash.com/photo-1592788174877-3f99727fd23d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80'
@@ -16,15 +15,14 @@ describe('Card Component', () => {
 			/>
 		)
 
-		// execution
-		const poster = screen.getByAltText('Movie Poster')
+		const poster = screen.getByAltText(/movie pOster/i)
 		const title = screen.getByText(/im a title/i)
 		const rating = screen.getByText(/67/)
 
-		// assertion
 		expect(poster).toBeInTheDocument()
 		expect(title).toBeInTheDocument()
 		expect(rating).toBeInTheDocument()
+		
 	})
 
 	it('Should', () => {

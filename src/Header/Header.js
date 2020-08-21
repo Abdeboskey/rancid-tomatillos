ea21a891bcfb962a6f83276d5d83040206dd23f7
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-08-20 19:41:33 -0600

    Destructure props and add onclick to login button #16

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import '../scss/_Header.scss'
 
-const Header = (props) => {
+const Header = ({ loginStatus, showLoginPage }) => {
   return (
     <header>
 			<img src='https://media.giphy.com/media/KXBtTtm3kB8BO/giphy.gif'alt='Website Icon'/>
       <h1>Rancid Tomatillos</h1>
       {
-        props.loginStatus && 
+        loginStatus && 
         <>
           <p>Hello, username!</p>
           <button>Log Out</button>
         </>  
       }
-      {!props.loginStatus && <button>Log In</button>}
+      {!loginStatus && <button onClick={showLoginPage}>Log In</button>}
     </header>
   )
 }

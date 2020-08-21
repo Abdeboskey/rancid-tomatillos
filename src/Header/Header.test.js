1c7caf788bfcff5548bcbec0bb240bcff69d662a
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-08-20 21:58:42 -0600

    Create and pass functional tests #23

--- src/Header/Header.test.js
+++ src/Header/Header.test.js
@@ -1,23 +1,53 @@
 import React from 'react'
 import Header from './Header'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
 describe('Header Component', () => {
 
   it('should render the user\'s name when logged in', () => {
-
     render(
       <Header 
         name='Georgina'
         loginStatus={true}
       />
     ) 
-  
-    const name = screen.getByText(/georgina/i)
-
-    expect(name).toBeInTheDocument()
     
+    const name = screen.getByText(/georgina/i)
+    
+    expect(name).toBeInTheDocument()
   })
   
+  it('should allow a user to log in', () => { 
+    const mockShowLoginPage = jest.fn()
+    render(
+      <Header 
+        name='Georgina'
+        loginStatus={false}
+        showLoginPage={mockShowLoginPage}
+      />
+    )
+    
+    const button = screen.getByRole('button')
+    fireEvent.click(button)
+    
+    expect(mockShowLoginPage).toBeCalledTimes(1)
+  })
+  
+  it('should allow a user to log out', () => {
+    const mockLogOut = jest.fn()
+    render(
+      <Header 
+        name='Georgina'
+        loginStatus={true}
+        logOut={mockLogOut}
+      />
+    )
+      
+    const button = screen.getByRole('button')
+    fireEvent.click(button)
+
+    expect(mockLogOut).toBeCalledTimes(1)
+  })
+
 })

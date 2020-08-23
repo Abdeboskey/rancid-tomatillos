3b18ef7f20acf61ee5bbfba522b2f39ba7b06ad2
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-08-23 14:16:57 -0600

    Create file and arcitechtural pesudo #31

---
+++ src/Movie-details/MovieDetails.js
@@ -0,0 +1,23 @@
+// This is our movie details component
+// It is a Class-Component
+// It should display all of the details about a movie, including:
+
+// MovieDetails Component
+// ---> Backdrop (presented as a kind of Billboard-style header?)
+// ---> Tagline (if exists, overlaid on backdrop)
+
+// Ratings
+// ---> Poster
+// ---> Title
+// ---> Release Date
+// ---> Avarage Rating
+// ---> User Rating
+
+// Details
+// ---> Overview (Synopsis?)
+// ---> Genre (single or list)
+// ---> Budget
+// ---> Revenue
+// ---> Runtime
+
+// MovieDetails components will also need to store a unique ID to add user's ratings (The ID is provided)

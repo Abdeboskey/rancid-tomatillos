45cc6d15c81bc468b8233529423c416a3d2fe5e7
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-08-23 14:58:29 -0600

    Create constructor and initial methods #31

--- src/Movie-details/MovieDetails.js
+++ src/Movie-details/MovieDetails.js
@@ -2,7 +2,9 @@
 // It is a Class-Component
 // It should display all of the details about a movie, including:
 
-// MovieDetails Component
+////////// MovieDetails Component
+
+// Billboard
 // ---> Backdrop (presented as a kind of Billboard-style header?)
 // ---> Tagline (if exists, overlaid on backdrop)
 
@@ -21,3 +23,39 @@
 // ---> Runtime
 
 // MovieDetails components will also need to store a unique ID to add user's ratings (The ID is provided)
+
+import React, { Component } from 'react'
+import Ratings from '../Ratings/Ratings'
+import Details from '../Details/Details'
+import { getMovieDetails } from '../apiCalls'
+import '../scss/_MovieDetails.scss'
+
+class MovieDetails extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      movieId: this.props.id,
+      title: '',
+    }
+  }
+  
+  componentDidMount() {
+    getMovieDetails(this.state.movieId)
+      .then(data => setMovieInfo())
+  }
+
+  setMovieInfo() {
+    this.setState({})
+  }
+
+  render() {
+    return (
+      <>
+        <Billboard />
+        <Ratings />
+        <Details />
+      </>
+    )
+  }
+}
+export default MovieDetails

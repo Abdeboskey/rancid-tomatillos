097497181bd5f5f1f415a1e7ca8c1af1e16816c5
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-08-22 18:15:30 -0600

    Create test for invalid credentials #14

--- src/Login/Login.test.js
+++ src/Login/Login.test.js
@@ -1,39 +1,31 @@
 import React from 'react'
 import Login from './Login'
-import { screen, render, fireEvent } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom'
+import { submitLoginCredentials } from '../apiCalls'
+jest.mock('../apiCalls.js')
 
 describe('Login Component', () => {
 
-  it('should fire handlesubmit when a user tries to log in', () => {
+  it('should fire handleSubmit when a user tries to log in', () => {
     
   })
 
-  // it('should notify the user if login credentials are INVALID', () => {
-  //   // setup
-  //   // render the login component
-  //   render(<Login />)
+  it('should notify the user if login credentials are INVALID', async () => {
+		submitLoginCredentials.mockResolvedValueOnce(
+			{ "error": "Username or password is incorrect" }
+		)
 
-  //   // execution
-  //   // find the inputs and the button
-  //   // fire an event by clicking the button with incorrect credentials
+    const { getByRole, findByText } = render(<Login />)
+		
+    const button = getByRole('button')
+		fireEvent.click(button)
+    const errorMessage = await findByText(
+			/invalid login information, please try again./i
+		)
 
-  //   const usernameInput = screen.getByRole('textbox', { id: /username/i })
-  //   const passwordInput = screen.getByRole('textbox', { id: /password/i })
-  //   const button = screen.getByRole('button')
-  //   fireEvent.change(usernameInput, { target: { value: 'tinzel@turing.io' } })
-  //   fireEvent.change(passwordInput, { target: { value: 'zcvb' } })
-  //   fireEvent.click(button)
-  //   const errorMessage = screen.getByText(
-  //     /invalid login information, please try again./i
-  //   );
-    
-  //   // assertion 
-  //   // make sure that the error renders on the page
-
-  //   expect(errorMessage).toBeInTheDocument()
-
-  // })
+    expect(errorMessage).toBeInTheDocument()
+  })
 
   it('should clear the input fields upon clicking submit', () => {
 

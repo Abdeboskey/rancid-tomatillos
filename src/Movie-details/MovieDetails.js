12ed10521a925f565f4e33450ad92dc246a3910a
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-08-23 15:12:36 -0600

    Update state and create setmovieinfo method #31

--- src/Movie-details/MovieDetails.js
+++ src/Movie-details/MovieDetails.js
@@ -36,16 +36,44 @@ class MovieDetails extends Component {
     this.state = {
       movieId: this.props.id,
       title: '',
+      poster: '',
+      backdrop: '',
+      releaseDate: '',
+      overview: '',
+      averageRating: '',
+      genres: [],
+      budget: 'number',
+      revenue: 'number',
+      runtime: 'number',
+      tagline: '',
+      videos: [],
+      error: '',
     }
   }
   
   componentDidMount() {
     getMovieDetails(this.state.movieId)
-      .then(data => setMovieInfo())
+      .then(data => setMovieInfo(data))
+      .catch(error => {
+        console.log(error)
+        this.setState({ error: error })
+      })
   }
 
-  setMovieInfo() {
-    this.setState({})
+  setMovieInfo({ movie }) {
+    this.setState({
+      title: movie.title,
+      poster: movie.poster_path,
+      backdrop: movie.backdrop_path,
+      releaseDate: movie.release_date,
+      overview: movie.overview,
+      averageRating: movie.average_rating,
+      genres: movie.genres,
+      budget: movie.budget,
+      revenue: movie.revenue,
+      runtime: movie.runtime,
+      tagline: movie.tagline,
+    })
   }
 
   render() {

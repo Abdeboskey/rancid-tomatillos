4c92672bbafd0ec833abe7d5ae747d23c64db020
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-08-23 17:28:50 -0600

    Add imports and proptypes for videos component #53

---
+++ src/Videos/Videos.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import '../scss/Videos.scss'
+
+const Videos = ({ videos }) => {
+	return (
+
+	);
+}
+
+export default Videos
+
+Videos.propTypes = {
+	videos: PropTypes.array.isRequired,
+}

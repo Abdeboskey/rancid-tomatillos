2d7efcfc40f98549195e30cbc01b576c3b50ebc8
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-08-23 19:29:30 -0600

    Add iframe tag for video #57

--- src/Video/Video.js
+++ src/Video/Video.js
@@ -2,12 +2,22 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import '../scss/_Videos.scss'
 
-const Video = ({  }) => {
-
+const Video = ({ videoId, key }) => {
+	return (
+		<iframe
+			title={videoId}
+			width='560'
+			height='315'
+			src={`https://www.youtube.com/embed/${key}`}
+			frameborder='0'
+			allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+			allowfullscreen>
+		</iframe>
+	)
 }
 
 export default Video
 
 Video.propTypes = {
-
+	key: PropTypes.string.isRequired
 }

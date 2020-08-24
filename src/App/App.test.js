82d92076c92f0ae5f20454b74786629081ccfc75
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-08-24 17:52:20 -0600

    Change quotes from double to single #74

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -14,28 +14,28 @@ describe('App Component', () => {
 		getMovies.mockResolvedValueOnce({
 			movies: [
 				{
-					"id": 1,
-					"poster_path": "www.posterpath.com/poster1",
-					"backdrop_path": "www.posterpath.com/backdrop1",
-					"title": "Brave Little Toasty",
-					"average_rating": 37,
-					"release_date": "1988-07-16"
+					'id': 1,
+					'poster_path': 'www.posterpath.com/poster1',
+					'backdrop_path': 'www.posterpath.com/backdrop1',
+					'title': 'Brave Little Toasty',
+					'average_rating': 37,
+					'release_date': '1988-07-16'
 				},
 				{
-					"id": 2,
-					"poster_path": "www.posterpath.com/poster2",
-					"backdrop_path": "www.posterpath.com/backdrop2",
-					"title": "Hack3r5",
-					"average_rating": 2,
-					"release_date": "2020-08-15"
+					'id': 2,
+					'poster_path': 'www.posterpath.com/poster2',
+					'backdrop_path': 'www.posterpath.com/backdrop2',
+					'title': 'Hack3r5',
+					'average_rating': 2,
+					'release_date': '2020-08-15'
 				},
 				{
-					"id": 3,
-					"poster_path": "www.posterpath.com/poster3",
-					"backdrop_path": "www.posterpath.com/backdrop3",
-					"title": "Jurassic Perks",
-					"average_rating": 6,
-					"release_date": "1994-03-24"
+					'id': 3,
+					'poster_path': 'www.posterpath.com/poster3',
+					'backdrop_path': 'www.posterpath.com/backdrop3',
+					'title': 'Jurassic Perks',
+					'average_rating': 6,
+					'release_date': '1994-03-24'
 				}
 			]
 		})
@@ -66,7 +66,5 @@ describe('App Component', () => {
 
 		expect(noMoviesMessage).toBeInTheDocument()
 	})
-
-	// will there ever be a case where there is NO response from the server??
-
+	
 })

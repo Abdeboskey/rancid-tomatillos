68f9c2ac1d1db347b458072ef189874d3c677d97
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-08-24 17:01:03 -0600

    Add header with video type #69

--- src/Video/Video.js
+++ src/Video/Video.js
@@ -2,18 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import '../scss/_Videos.scss'
 
-const Video = ({ videoId, movieKey }) => {
+const Video = ({ videoId, movieKey, type }) => {
 	return (
-		<iframe
-			title={videoId}
-			width='560'
-			height='315'
-			src={`https://www.youtube.com/embed/${movieKey}`}
-			frameBorder='0'
-			allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
-			allowFullScreen
-			>
-		</iframe>
+		<>
+			<h3>{type}</h3>
+			<iframe
+				title={videoId}
+				width='560'
+				height='315'
+				src={`https://www.youtube.com/embed/${movieKey}`}
+				frameBorder='0'
+				allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+				allowFullScreen
+				>
+			</iframe>
+		</>
 	)
 }
 

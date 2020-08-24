e897ab68a6e49cbbf94a4e8b01d069a63c1d5975
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-08-24 15:26:19 -0600

    Change login logout button to use clapboard #44

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,22 +1,43 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import clapboard from '../assets/clapboard.png'
 import '../scss/_Header.scss'
 
 const Header = ({ loginStatus, showLoginPage, logOut, name }) => {
   return (
     <header>
-			<img src='https://media.giphy.com/media/KXBtTtm3kB8BO/giphy.gif'alt='Website Icon'/>
+      <img
+        className="logo"
+        src="https://media.giphy.com/media/KXBtTtm3kB8BO/giphy.gif"
+        alt="Website Icon"
+      />
       <h1>Rancid Tomatillos</h1>
-      {
-        loginStatus && 
+      {loginStatus && (
         <>
           <p>Hello, {name}</p>
-          <button onClick={logOut}>Log Out</button>
-        </>  
-      }
-      {!loginStatus && <button onClick={showLoginPage}>Log In</button>}
+          <div className="button">
+            <img
+              src={clapboard}
+              onClick={logOut}
+              alt="Movie-Production Clapboard"
+            />
+            <p onClick={logOut}>Log Out</p>
+          </div>
+        </>
+      )}
+      {!loginStatus && (
+        <div className="button">
+          <img
+            className="button"
+            src={clapboard}
+            onClick={showLoginPage}
+            alt="Movie-Production Clapboard"
+          />
+          <p onClick={showLoginPage}>Log In</p>
+        </div>
+      )}
     </header>
-  )
+  );
 }
 
 export default Header

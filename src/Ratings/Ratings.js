01f9decaa13476a55f876704862de374feabed2c
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-08-26 17:15:05 -0600

    Add form to submit user rating #81

--- src/Ratings/Ratings.js
+++ src/Ratings/Ratings.js
@@ -12,6 +12,12 @@ const Ratings = ({ title, poster, releaseDate, averageRating, userRating }) => {
         <p>Average Rating: {averageRating} / 10</p>
 				{userRating && <p>Your Rating: {userRating.rating} / 10</p>}
       </article>
+			<form className='MovieDetails-NewRating'>
+				<input id='rating' type='range' min='0' max='10'>
+				</input>
+				<label htmlFor='rating'>IM A LABEL</label>
+				<button>Submit Rating</button>
+			</form>
     </section>
   ) 
 }
@@ -22,5 +28,5 @@ Ratings.propTypes = {
   title: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
   releaseDate: PropTypes.string.isRequired,
-  averageRating: PropTypes.number.isRequired,
+  averageRating: PropTypes.number.isRequired
 }

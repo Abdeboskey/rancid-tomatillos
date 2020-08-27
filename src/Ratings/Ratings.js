27fa30f0b54913384b5d20ee69767b0a2037ef40
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-08-26 19:50:43 -0600

    Call methods to post a rating #81

--- src/Ratings/Ratings.js
+++ src/Ratings/Ratings.js
@@ -2,7 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import '../scss/_Ratings.scss'
 
-const Ratings = ({ title, poster, releaseDate, averageRating, userRating }) => {
+const Ratings = ({ userId, movieId, title, poster, releaseDate, averageRating, userRating, submitRating, handleUserRatingInput }) => {
+
   return (
     <section className='MovieDetails-Ratings'>
       <img src={poster} alt='Movie Poster' />
@@ -10,14 +11,35 @@ const Ratings = ({ title, poster, releaseDate, averageRating, userRating }) => {
         <h2>{title}</h2>
         <p>{releaseDate.slice(-4)}</p>
         <p>Average Rating: {averageRating} / 10</p>
-				{userRating && <p>Your Rating: {userRating.rating} / 10</p>}
+				{typeof userRating === 'object' && 
+					<p>Your Rating: {userRating.rating} / 10</p>}
+				{typeof userRating === 'string' && 
+					<p>Your Rating: {userRating} / 10</p>}
       </article>
-			<form className='MovieDetails-NewRating'>
-				<input id='rating' type='range' min='0' max='10'>
-				</input>
-				<label htmlFor='rating'>IM A LABEL</label>
-				<button>Submit Rating</button>
-			</form>
+				<form
+					className='MovieDetails-NewRating'
+					onSubmit={(event) => submitRating(userId, userRating, movieId, event)}
+				>
+					<input
+						id='rating'
+						type='range'
+						min='0'
+						max='10'
+						value={userRating}
+						onChange={(event) => handleUserRatingInput(event)}
+					>
+					</input>
+					{typeof userRating === 'object' &&
+						<label htmlFor='rating'>{userRating.rating} / 10 </label>
+					}
+					{typeof userRating === 'number' &&
+						<label htmlFor='rating'>{userRating} / 10 </label>
+					}
+					<button type='submit'>Submit Rating</button>
+				</form>
+			{/* {userRating &&
+				<button onClick={() => deleteRating}>Rate again!</button>
+			} */}
     </section>
   ) 
 }

ba8f884721853a761568bb3d34a90827acdea64c
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-08-28 13:27:28 -0600

    Change first test to show correct content when logged out #88

--- src/Card/Card.test.js
+++ src/Card/Card.test.js
@@ -3,45 +3,35 @@ import Card from './Card'
 import { render, screen, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
+import { BrowserRouter } from 'react-router-dom'
+
 describe('Card Component', () => {
 
-	it('should show the correct content when rendered', () => {
-		const mockShowMovieDetails = jest.fn()
+	it('should show the correct content when rendered logged out', () => {
 		render(
-			<Card
-				movieId={4}
-				title='Im a title'
-				poster='www.posterpath.com/poster'
-				rating={67}
-				showMovieDetails={mockShowMovieDetails}
-			/>
+			<BrowserRouter>
+				<Card
+					movieId={4}
+					title='Im a title'
+					poster='www.posterpath.com/poster'
+					rating={10}
+					userRating={undefined}
+					isLoggedIn={false}
+				/>
+			</BrowserRouter>
 		)
 
-		const poster = screen.getByAltText(/movie pOster/i)
 		const title = screen.getByText(/im a title/i)
-		const rating = screen.getByText(/67/)
+		const rating = screen.getByText(/10/)
+		const poster = screen.getByAltText(/movie pOster/i)
 
-		expect(poster).toBeInTheDocument()
 		expect(title).toBeInTheDocument()
 		expect(rating).toBeInTheDocument()
+		expect(poster).toBeInTheDocument()
 	})
 
 	it('should show movie details when poster is clicked', () => {
-		const mockShowMovieDetails = jest.fn()
-		render(
-			<Card
-				movieId={35}
-				title='The Adventures of Howard and Finn'
-				poster='www.posterpath.com/poster'
-				rating={1000}
-				showMovieDetails={mockShowMovieDetails}
-			/>
-		)
-		const poster = screen.getByAltText(/movie poster/i)
-		fireEvent.click(poster)
 
-		expect(mockShowMovieDetails).toBeCalledTimes(1)
-		expect(mockShowMovieDetails).toBeCalledWith(35)
 	})
 
 })

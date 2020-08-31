fc8f4ec31a17a317401af0ff63f27152c119eedc
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-08-31 16:09:04 -0600

    Pass add favorite as props to card #105

--- src/Movies/Movies.js
+++ src/Movies/Movies.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Card from '../Card/Card'
 import '../scss/_Movies.scss'
 
-const Movies = ({ movies, formatAverageRating, userRatings, isLoggedIn }) => {
+const Movies = ({ movies, formatAverageRating, userRatings, isLoggedIn, addFavorite }) => {
 	const moviesList = movies.map(movie => {
 		const userRating = userRatings.find(rating => rating.movie_id === movie.id)
 		return (
@@ -15,6 +15,7 @@ const Movies = ({ movies, formatAverageRating, userRatings, isLoggedIn }) => {
 				rating={formatAverageRating(movie.average_rating)}
 				userRating={userRating}
 				isLoggedIn={isLoggedIn}
+				addFavorite={addFavorite}
 			/>
 		)
 	})

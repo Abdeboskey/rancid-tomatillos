78af721c997de46ea1a826101d9b58c098ffeb6b
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-08-31 17:25:56 -0600

    Add star icon to render in ratings #105

--- src/Ratings/Ratings.js
+++ src/Ratings/Ratings.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import '../scss/_Ratings.scss'
+import star from '../assets/star.png'
 
 const Ratings = ( props ) => {
-
   return (
     <section className='MovieDetails-Ratings'>
       <img src={props.poster} alt='Movie Poster' />
       <article>
         <h2>{props.title}</h2>
+				{props.isLoggedIn &&
+					<input
+						type='image'
+						alt='Star Icon'
+						src={star}
+						className='favorite'
+						onClick={(event) => props.changeFavoriteStatus(props.movieId, event)}
+					></input>}
         <p className='release-date'>{props.releaseDate.slice(-4)}</p>
         <p className='rating'>
           <b>Average Rating:</b><br/> {props.averageRating} / 10

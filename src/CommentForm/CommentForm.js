7add3b290df6c8ca76346ade83906e73e65e6f18
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-01 00:22:34 -0600

    Remove comments #107

--- src/CommentForm/CommentForm.js
+++ src/CommentForm/CommentForm.js
@@ -1,12 +1,6 @@
 import React from 'react'
 import '../scss/_CommentForm.scss'
 
-// will need to update state in MovieDetails with the values of the fields. 
-//// use handleCommentInput() passed as props from movie details. 
-//// state will live in MovieDetails.
-//// will fire postComment() on submit.
-//// postComment() is passed down from MovieDetails.
-
 const CommentForm = ({ comment, handleCommentInput, addComment }) => {
   return (
     <form 

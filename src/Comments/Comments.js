cbae625166ce9a658240b4ee3cfd95e024d85a08
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-01 00:22:44 -0600

    Import scss

--- src/Comments/Comments.js
+++ src/Comments/Comments.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Comment from '../Comment/Comment'
+import '../scss/_Comments.scss'
 
 const Comments = ({ comments }) => {
   const commentsList = comments.map(comment => {

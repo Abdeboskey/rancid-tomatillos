7b2c2a26fa0e4aa65520cf0bb1f00740c5d85dec
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-01 12:24:48 -0600

    Refactor to be a controlled form #107

--- src/CommentForm/CommentForm.js
+++ src/CommentForm/CommentForm.js
@@ -1,22 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 import '../scss/_CommentForm.scss'
 
-const CommentForm = ({ comment, handleCommentInput, addComment }) => {
-  return (
-    <form 
-      className='CommentForm'
-      onSubmit={event => addComment(event)}
-    >
-      <input 
-        className='Comment-input'
-        type='text'
-        placeholder='Add Comment Here'
-        value={comment}
-        onChange={event => handleCommentInput(event)}
-      />
-      <button type='submit'>Comment!</button>
-    </form>
-  )
+class CommentForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      comment: '',
+    }
+  }
+
+  handleCommentInput = (event) => {
+    const inputValue = event.target.value
+    this.setState({ comment: inputValue })
+  }
+
+  resetCommentInput = () => {
+    this.setState({ comment: '' })
+  } 
+
+  render() {
+    return (
+      <form 
+        className='CommentForm'
+        onSubmit={event => {
+          this.props.addComment(event, this.state.comment)
+          this.resetCommentInput()
+        }}
+      >
+        <input 
+          className='Comment-input'
+          type='text'
+          placeholder='Add Comment Here'
+          value={this.state.comment}
+          onChange={event => this.handleCommentInput(event)}
+        />
+        <button type='submit'>Comment!</button>
+      </form>
+    )
+  }
 
 }
 

a856a606e67c4d5ae46c6e7952b1a39267b8209a
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-08-31 20:55:46 -0600

    Create initial component #107

---
+++ src/CommentForm/CommentForm.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+// will need to update state in MovieDetails with the values of the fields. 
+//// use handleCommentInput() passed as props from movie details. 
+//// state will live in MovieDetails.
+//// will fire postComment() on submit.
+//// postComment() is passed down from MovieDetails.
+
+const CommentForm = ({ comment, handleCommentInput, addComment }) => {
+  return (
+    <form 
+      className='CommentForm'
+      onSubmit={event => addComment(event)}
+    >
+      <input 
+        className='Comment-input'
+        type='text'
+        value={comment}
+        onChange={event => handleCommentInput(event)}
+      />
+      <button type='submit'>Comment!</button>
+    </form>
+  )
+
+}
+
+export default CommentForm
